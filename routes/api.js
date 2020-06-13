6df9991854428dd3414144a53da0b42ddcf114ef
Alex B <abailon949@gmail.com>
2020-06-12 22:23:38 -0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -31,4 +31,8 @@ router.get("/api/transaction", (req, res) => {
     });
 });
 
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
 module.exports = router;
